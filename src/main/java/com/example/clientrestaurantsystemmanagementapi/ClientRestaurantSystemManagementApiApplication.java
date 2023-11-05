e4973a589868aa862360c4ff7dede90349f3f8db
marcinkida97 <mkida97@gmail.com>
2023-11-05 16:05:31 +0100

    Project initialization

---
+++ src/main/java/com/example/clientrestaurantsystemmanagementapi/ClientRestaurantSystemManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientrestaurantsystemmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientRestaurantSystemManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientRestaurantSystemManagementApiApplication.class, args);
+    }
+
+}
